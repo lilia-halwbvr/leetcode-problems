0c23e3e3fce1d06e0d046d1e4f829df21ff45734
Liliia <halwbvr@yandex.ru>
2023-11-03 00:41:24 +0400

    1365.HowManyNumbersAreSmallerThantheCurrentNumber.js

---
+++ 1365.HowManyNumbersAreSmallerThantheCurrentNumber.js
@@ -0,0 +1,18 @@
+var smallerNumbersThanCurrent = function(nums) {
+    let result = []
+     for(let i = 0; i < nums.length; i++) {
+         let smallerNums = 0
+         for(let j = 0; j < nums.length; j++) {
+             if(nums[j] < nums[i]) {
+                 smallerNums++
+             }
+         }
+         result.push(smallerNums)
+
+     }
+     return result
+};
+
+console.log(smallerNumbersThanCurrent([8,1,2,2,3])) //[4,0,1,1,3]
+console.log(smallerNumbersThanCurrent([6,5,4,8])) //[2,1,0,3]
+console.log(smallerNumbersThanCurrent([7,7,7,7])) //[0,0,0,0]
