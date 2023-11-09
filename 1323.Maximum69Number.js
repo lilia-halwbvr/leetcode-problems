fc46d15d8973f525134c3a0911ce09a625140491
Liliia <halwbvr@yandex.ru>
2023-11-09 15:58:17 +0400

    1323

---
+++ 1323.Maximum69Number.js
@@ -0,0 +1,15 @@
+var maximum69Number  = function(num) {
+    let a = Array.from(String(num))
+    for(let i = 0; i < a.length; i++) {
+        if(a[i] == 6) {
+            a.splice(i, 1, "9");
+           break
+        }
+
+    }
+    return Number(a.join(''))
+};
+
+console.log(maximum69Number(9669))//9969
+console.log(maximum69Number(9996))//9999
+console.log(maximum69Number(9999)) //9999
