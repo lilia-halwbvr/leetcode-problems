55791b38c47df1fdd85a557d8c83392240c69868
Liliia <halwbvr@yandex.ru>
2023-12-06 14:39:44 +0400

    326.PowerofThree.js

--- 217.ContainsDuplicate.js
+++ 217.ContainsDuplicate.js
@@ -2,7 +2,6 @@ var containsDuplicate = function(nums) {
     for(let i = 0; i <= nums.length; i++) {
         for(let j = i + 1; j < nums.length; j++) {
             if(nums[i] == nums[j]) {
-                console.log(nums[i], nums[j])
                 return true
             }
         }
---
+++ 326.PowerofThree.js
@@ -0,0 +1,14 @@
+var isPowerOfThree = function(n) {
+    if(n < 3) return false
+    while(n % 3 === 0) {
+        n /= 3
+    }
+
+    return n === 1
+
+    
+};
+
+console.log(isPowerOfThree(27)) //true
+console.log(isPowerOfThree(0)) //false
+console.log(isPowerOfThree(-1)) //false
