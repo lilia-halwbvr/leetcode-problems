b153a3d63148c0e7c989b855b0e1d6e6307dbae1
Liliia <halwbvr@yandex.ru>
2023-12-18 18:00:03 +0400

    1408.StringMatchinginanArray.js

---
+++ 1408.StringMatchinginanArray.js
@@ -0,0 +1,18 @@
+var stringMatching = function(words) {
+    let result = []
+    for(let i = 0; i < words.length; i++) {
+       for(let j = 0; j < words.length; j++) {
+            if(i === j) continue
+            if(words[j].includes(words[i])) {
+                result.push(words[i])
+                break
+            }
+        }
+    }       
+
+    return result
+};
+
+console.log(stringMatching(["mass","as","hero","superhero"])) //["as","hero"]
+console.log(stringMatching(["leetcode","et","code"])) //["et","code"]
+console.log(stringMatching(["blue","green","bu"])) //[]
