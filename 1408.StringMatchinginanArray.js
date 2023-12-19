d0075ff068cf99f62dfc02ef6f767a0b14ba0a1e
Liliia <halwbvr@yandex.ru>
2023-12-19 17:40:44 +0400

    1408.StringMatchinginanArray.js

--- 1408.StringMatchinginanArray.js
+++ 1408.StringMatchinginanArray.js
@@ -2,8 +2,7 @@ var stringMatching = function(words) {
     let result = []
     for(let i = 0; i < words.length; i++) {
        for(let j = 0; j < words.length; j++) {
-            if(i === j) continue
-            if(words[j].includes(words[i])) {
+            if(i !== j && words[j].includes(words[i])) {
                 result.push(words[i])
                 break
             }
