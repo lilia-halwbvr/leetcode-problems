74f4bdabae2b9247cf90a25f45ab291e1a1863d9
Liliia <halwbvr@yandex.ru>
2024-01-18 16:51:00 +0400

    70.ClimbingStairs.js

---
+++ 70.ClimbingStairs.js
@@ -0,0 +1,20 @@
+var climbStairs = function(n) {
+
+    if(n < 2) return 1
+
+    let firstStep = 1
+    let secondStep = 2
+    let result = 0
+
+    for(let i = 2; i <= n; i++) {
+        result = firstStep + secondStep
+        firstStep = secondStep
+        secondStep = result
+    }
+
+    return result
+};
+
+console.log(climbStairs(2)) //2
+console.log(climbStairs(3)) //3
+console.log(climbStairs(1)) //1
