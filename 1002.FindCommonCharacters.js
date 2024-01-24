97492bcff582158df27790ca8a264a0259364756
Liliia <halwbvr@yandex.ru>
2024-01-24 18:21:17 +0400

    1002.FindCommonCharacters.js

---
+++ 1002.FindCommonCharacters.js
@@ -0,0 +1,32 @@
+var commonChars = function(words) {
+    let answer = []
+    let wordsInArr = []
+
+    for(let i = 0; i < words.length; i++) {
+        let map = new Map()
+        for(let j = 0; j < words[i].length; j++) {
+            map.set(words[i][j], (map.get(words[i][j]) || 0) + 1)
+        }
+        wordsInArr.push(map)
+    }
+
+    let firstWord = wordsInArr[0]
+    for(let [key, value] of firstWord) {
+        for(let i = 1; i < wordsInArr.length; i++) {
+            firstWord.set(key, Math.min(firstWord.get(key), wordsInArr[i].get(key) || 0))
+        }
+    }
+
+    firstWord.forEach((value, key) => {
+        for (let i = 0; i < value; i++) {
+          answer.push(key);
+        }
+      });
+   
+return answer
+};
+
+ 
+
+console.log(commonChars(["bella","label","roller"])) //["e","l","l"]
+console.log(commonChars(["cool","lock","cook"])) //["c","o"]
