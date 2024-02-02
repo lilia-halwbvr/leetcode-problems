c5d2e493033be658240af1c209e23cd732030c5e
Liliia <halwbvr@yandex.ru>
2024-02-02 16:07:57 +0400

    1291.SequentialDigits.js

---
+++ 1291.SequentialDigits.js
@@ -0,0 +1,20 @@
+var sequentialDigits = function(low, high) {
+    let answer = []
+
+    for(let i = 1; i <= 9; i++) {
+        let num = i
+        for(let j = i + 1; j <= 9; j++) {
+            num = num * 10 + j
+            if(num >= low && num <= high) {
+                answer.push(num)
+            }
+        }
+
+        
+    }
+
+    return answer.sort((a, b) => a - b)
+};
+
+console.log(sequentialDigits(100, 300)) //[123,234]
+console.log(sequentialDigits(1000, 13000)) //[1234,2345,3456,4567,5678,6789,12345]
