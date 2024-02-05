3f7925e0f77acc6976a831c56bd6d81d50c7bc7b
Liliia <halwbvr@yandex.ru>
2024-02-05 14:05:45 +0400

    387.FirstUniqueCharacterInAString.js

--- 387.FirstUniqueCharacterInAString.js
+++ 387.FirstUniqueCharacterInAString.js
@@ -1,18 +1,15 @@
 // Given a string s, find the first non-repeating character in it and return its index. If it does not exist, return -1.
 
 var firstUniqChar = function(s) {
-    for(let i = 0; i <= s.length; i++) {
-        for(let j = i + 1; j <= s.length; j++) {
-            console.log(s[i], s[j])
-            if(s[i] == s[j]) {
-                break
-            }
-            // return i
-        }
-        
-        
-    }
-    return -1
+   let map = new Map()
+
+   for(letter of s) {
+        map[letter] = (map[letter] || 0) + 1;
+   }
+   for(let i of Object.keys(map)) {
+       if(map[i] === 1) return s.indexOf(i)
+   }
+   return -1
 };
 
 
