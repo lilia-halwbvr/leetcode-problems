1ef02a7614264fa3ea2f4088109e2b1a76e4163b
Liliia <lilia_halwbvr@icloud.com>
2024-08-08 14:06:54 +0400

    824.CountPairsWhoseSumisLessthanTarget.js

---
+++ 824.CountPairsWhoseSumisLessthanTarget.js
@@ -0,0 +1,14 @@
+var countPairs = function (nums, target) {
+    let pairs = 0
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+            if (nums[i] + nums[j] < target) {
+                pairs++
+            }
+        }
+    }
+    return pairs
+};
+
+console.log(countPairs([-1, 1, 2, 3, 1], 2)) //3
+console.log(countPairs([-6, 2, 5, -2, -7, -1, 3], -2)) //10
