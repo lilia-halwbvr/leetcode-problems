25e51f21d8a207c842967c940416b1f0aa97b428
Liliia <lilia_halwbvr@icloud.com>
2024-08-21 11:28:58 +0400

    521.LongestUncommonSubsequenceI.js

---
+++ 521.LongestUncommonSubsequenceI.js
@@ -0,0 +1,6 @@
+var findLUSlength = function (a, b) {
+    return a == b ? -1 : Math.max(a.length, b.length)
+};
+
+console.log(findLUSlength("aba", "cdc")) //3
+console.log(findLUSlength("aaa", "bbb")) //3
